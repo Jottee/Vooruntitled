5eb4df972f48476325cfb858d4c8091b079a7060
Jotte <josooooooo@gmail.com>
2016-03-09 11:53:58 +0100

    eerste commit

---
+++ src/testpackage/Calculator.java
@@ -0,0 +1,15 @@
+package testpackage;
+
+/**
+ * Created by Jotte on 9-3-2016.
+ */
+public class Calculator {
+
+    public Calculator() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
